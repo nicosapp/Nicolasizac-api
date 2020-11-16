3ce83070e3a13bc1f2c4555f543678fbd657d3b1
Niz <62140571+nicosapp@users.noreply.github.com>
2020-11-16 18:46:52 +0100

    remove test

--- routes/api.php
+++ routes/api.php
@@ -48,7 +48,3 @@ Route::group(['middleware' => 'guest', 'prefix' => 'contact', 'namespace' => 'Co
 Route::group(['prefix' => 'media', 'namespace' => 'Media'], function () {
   Route::get('config', 'MediaConfigController@index');
 });
-
-Route::get('test', function () {
-  return response()->json(['message' => 'success']);
-});
